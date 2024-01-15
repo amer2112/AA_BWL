405f639f36534825a08bf609cabc2a42a5f2f945
amer2112 <151033635+amer2112@users.noreply.github.com>
2024-01-15 13:11:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var models = ["model1", "model2", /* add more models as needed */];
 var definitions = ["def. model1", "def. model2", /* add more definitions as needed */];
 var currentIndex = 0;
+var isCardFlipped = false;
+
+function toggleCardFlip() {
+    if (!isCardFlipped) {
+        flipCard();
+    } else {
+        hideDefinition();
+        isCardFlipped = false;
+    }
+}
 
 function flipCard() {
     var cardInner = document.querySelector('.card-inner');
@@ -9,11 +19,11 @@ function flipCard() {
     setTimeout(function () {
         updateCardContent();
         cardInner.style.transform = 'rotateY(0deg)';
+        isCardFlipped = true;
     }, 500);
 }
 
 function updateCardContent() {
-    currentIndex = (currentIndex + 1) % models.length;
     document.getElementById('model-text').innerText = models[currentIndex];
     document.getElementById('definition-text').innerText = definitions[currentIndex];
 }
